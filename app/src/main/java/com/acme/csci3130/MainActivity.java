bde65e53bec55be7a3484ff0a01fc2b5da41e9c1
John Phillips <jh355248@dal.ca>
2017-05-06 13:59:41 -0300

    Added a input text field and a button. Once the user clicks the button, the Hello World text field's text is changed to what was entered in the text input field.

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,9 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitButton(View view) {
+        TextView tv = (TextView) findViewById(R.id.helloText);
+        EditText et = (EditText) findViewById(R.id.editText);
+        tv.setText(et.getText());
     }
 }
